a4f3cb9d9c9852a3b0c03ee54c9a05485657a584
prolyfix <marc.baudot@gmail.com>
2024-09-03 15:20:39 +0200

    mise à jour pour le JS!

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,4 @@ import './bootstrap.js';
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
-require('datatables.net-bs5/css/dataTables.bootstrap5.min.css');
-require('bootstrap-icons/font/bootstrap-icons.css');
-require('bootstrap');
-require('datatables');
+
---
+++ assets/controllers/time_controller.js
@@ -0,0 +1,9 @@
+import { Controller, StringMapObserver } from '@hotwired/stimulus';
+export default class extends Controller {
+    initialize() {
+    }
+    connect() {
+      alert("ici");
+    }
+
+}
