ba0ad79bd866cff5049ecef14fe3775d4a2499bf
prolyfix <marc.baudot@gmail.com>
2024-09-06 12:17:11 +0200

    Add Media to Project

--- src/Controller/Admin/ProjectCrudController.php
+++ src/Controller/Admin/ProjectCrudController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Media;
 use App\Entity\Project;
+use App\Form\MediaType;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
@@ -15,10 +18,12 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class ProjectCrudController extends AbstractCrudController
 {
-    public function __construct(private AdminUrlGenerator $crudUrlGenerator)
+    public function __construct(private AdminUrlGenerator $crudUrlGenerator, private RequestStack $requestStack, private EntityManagerInterface $em)
     {
     }
     public static function getEntityFqcn(): string
@@ -33,19 +38,59 @@ class ProjectCrudController extends AbstractCrudController
             TextField::new('name'),
             DateTimeField::new('creationDate')->hideOnForm(),
             AssociationField::new('thirdParty'),
-            AssociationField::new('tasks')->hideWhenCreating()->hideWhenUpdating()->setTemplatePath('admin/task/field.html.twig')
+            AssociationField::new('media')->hideOnIndex()->hideWhenCreating()->hideWhenUpdating()->setTemplatePath('admin/media/field.html.twig'),
+            AssociationField::new('tasks')->hideWhenCreating()->hideWhenUpdating()->setTemplatePath('admin/task/field.html.twig'),
         ];
     }
     public function configureActions(Actions $actions): Actions
     {
         $createTaskAction = Action::new('createTask', 'Create Task', 'fa fa-plus')
         ->linkToCrudAction('createTask');
+        $actionAddMedia = Action::new('addMedia', 'Add Media', 'fa fa-image')
+        ->linkToCrudAction('addMedia');
         return $actions
             // ...
             ->add(Crud::PAGE_INDEX, Action::DETAIL)
             ->add(Crud::PAGE_INDEX, $createTaskAction)
+            ->add(Crud::PAGE_DETAIL, $actionAddMedia)
         ;
     }
+    public function addMedia(EntityManagerInterface $em, Request $request)
+    {
+        $entityId = $request->get('entityId');
+        $entity = $em->getRepository(Project::class)->find($entityId);
+        $media = new Media();
+        $form = $this->createForm(MediaType::class, $media);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $media->setCommentable($entity);
+            $em->persist($media);
+            $em->flush();
+            return $this->redirectToRoute('admin',[
+                'crudAction' => 'detail',
+                'entityId' => $entityId,
+                'crudControllerFqcn' => 'App\Controller\Admin\ProjectCrudController',
+            ]);
+        }
+        return $this->render('admin/comment/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
+    } 
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        $request = $this->requestStack->getCurrentRequest();
+        if($request->query->get('entityId')){
+            $entityId = $request->query->get('entityId');
+            $entity = $this->em->getRepository(Project::class)->find($entityId);
+            $crud->setPageTitle(Crud::PAGE_DETAIL, $entity->getName());
+        }
+        //$crud->setPageTitle(Crud::PAGE_DETAIL, 'coucou');
+        return $crud;
+    }
+
+
     public function createTask(AdminContext $context): RedirectResponse
     {
         $projectId = $context->getEntity()->getPrimaryKeyValue();
